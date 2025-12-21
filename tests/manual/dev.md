8101bf2c6f394464fdce93e0a8921209d1903f39
Lucky Adams <proximadams@gmail.com>
2025-12-20 20:18:19 -0600

    fixed 2 broken links and added tests for bio hyperlinks

--- tests/manual/dev.md
+++ tests/manual/dev.md
@@ -1,3 +1,2 @@
-Confirm all images and videos load
 Confirm all videos can be paused
 Confirm all source links work
--- tests/unit/bio.spec.ts
+++ tests/unit/bio.spec.ts
@@ -5,28 +5,61 @@ var siteUrl: string = 'file://' + path.resolve('index.html');
 
 test.describe('bio', () => {
 	test.describe('social media links', () => {
-		test('itch link working', async ({ page }) => {
+		test('itch working', async ({ page }) => {
 			await page.goto(siteUrl);
 			await page.locator('#social-icon-links > a').first().click();
 			await expect(page).toHaveURL(/itch.io/)
 		});
 
-		test('github link working', async ({ page }) => {
+		test('github working', async ({ page }) => {
 			await page.goto(siteUrl);
 			await page.locator('#social-icon-links > a:nth-child(2)').click();
 			await expect(page).toHaveURL(/github.com/)
 		});
 
-		test('youtube link working', async ({ page }) => {
+		test('youtube working', async ({ page }) => {
 			await page.goto(siteUrl);
 			await page.locator('#social-icon-links > a:nth-child(3)').click();
 			await expect(page).toHaveURL(/youtube.com/)
 		});
 
-		test('linkedin link working', async ({ page }) => {
+		test('linkedin working', async ({ page }) => {
 			await page.goto(siteUrl);
 			await page.locator('#social-icon-links > a:nth-child(4)').click();
 			await expect(page).toHaveURL(/linkedin.com/)
+		});
+	});
+
+	test.describe.only('hyperlinks', () => {
+		test('winnipeg game collective working', async ({ page }) => {
+			await page.goto(siteUrl);
+			await page.getByRole('link', { name: 'Winnipeg Game Collective' }).click();
+			await expect(page).toHaveURL(/pegjam.com/)
+		});
+		test('sea birds working', async ({ page }) => {
+			await page.goto(siteUrl);
+			await page.getByRole('link', { name: 'Sea Birds: End of an Age' }).click();
+			await expect(page).toHaveURL(/Sea_Birds_End_of_an_Age/)
+		});
+		test('single feather working', async ({ page }) => {
+			await page.goto(siteUrl);
+			await page.getByRole('link', { name: 'Single Feather' }).click();
+			await expect(page).toHaveURL(/Single_Feather/)
+		});
+		test('rrc working', async ({ page }) => {
+			await page.goto(siteUrl);
+			await page.getByRole('link', { name: 'Red River College Polytechnic' }).click();
+			await expect(page).toHaveURL(/rrc.ca/)
+		});
+		test('umanitoba working', async ({ page }) => {
+			await page.goto(siteUrl);
+			await page.getByRole('link', { name: 'University of Manitoba' }).click();
+			await expect(page).toHaveURL(/umanitoba.ca/)
+		});
+		test('employees scholarship working', async ({ page }) => {
+			await page.goto(siteUrl);
+			await page.getByRole('link', { name: 'Employees Scholarship' }).click();
+			await expect(page).toHaveURL(/umanitoba.ca/)
 		});
 	});
 
