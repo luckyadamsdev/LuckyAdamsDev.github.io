4d5300a251d2a65ef73136bf051d3c6c9f5b86dd
Lucky Adams <proximadams@gmail.com>
2025-12-20 19:39:15 -0600

    test social media links work

--- tests/unit/bio.spec.ts
+++ tests/unit/bio.spec.ts
@@ -3,9 +3,28 @@ import path from 'path';
 
 var siteUrl: string = 'file://' + path.resolve('index.html');
 
-test('has title', async ({ page }) => {
-	await page.goto(siteUrl);
+test.describe('social media links', () => {
+	test('itch link working', async ({ page }) => {
+		await page.goto(siteUrl);
+		await page.locator('#social-icon-links > a').first().click();
+		await expect(page).toHaveURL(/itch.io/)
+	});
 
-	// Expect a title "to contain" a substring.
-	await expect(page).toHaveTitle('Portfolio Bio - Lucky Adams');
+	test('github link working', async ({ page }) => {
+		await page.goto(siteUrl);
+		await page.locator('#social-icon-links > a:nth-child(2)').click();
+		await expect(page).toHaveURL(/github.com/)
+	});
+
+	test('youtube link working', async ({ page }) => {
+		await page.goto(siteUrl);
+		await page.locator('#social-icon-links > a:nth-child(3)').click();
+		await expect(page).toHaveURL(/youtube.com/)
+	});
+
+	test('linkedin link working', async ({ page }) => {
+		await page.goto(siteUrl);
+		await page.locator('#social-icon-links > a:nth-child(4)').click();
+		await expect(page).toHaveURL(/linkedin.com/)
+	});
 });
