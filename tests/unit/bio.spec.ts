ff97c5c856339ea5e5faf9f00d4f569e17307501
Lucky Adams <proximadams@gmail.com>
2025-12-20 19:45:54 -0600

    remove .only

--- tests/unit/bio.spec.ts
+++ tests/unit/bio.spec.ts
@@ -29,7 +29,7 @@ test.describe('social media links', () => {
 	});
 });
 
-test.only('bio images load', async ({ page }) => {
+test('bio images load', async ({ page }) => {
 	await page.goto(siteUrl);
 	await expect(page).toHaveScreenshot('bio.png');
 });
